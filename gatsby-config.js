b7938b9f5c5eb2e6462930010e0c056d36670d8b
raffazizzi <raffaeleviglianti@gmail.com>
2022-09-22 14:13:49 -0400

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  siteMetadata: {
+    title: `Digital Publishing with Minimal Computing Project`,
+    description: `A template`,
+    author: `Course teachers`
+  },
+  plugins: [
+    `gatsby-plugin-material-ui`,
+    `gatsby-theme-ceteicean`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `src/content/tei`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `src/content/pages`,
+        name: `html`,
+      },
+    },
+  ],
+}
